fa2e413e2c5eeae503712c20a9d9bc3d959123e4
Đỗ Đình Duy <dodinhduy203@gmail.com>
2024-09-15 12:57:07 +0700

    ud1

---
+++ index.py
@@ -0,0 +1,13 @@
+# nhập điểm toán 
+toan = int(input("Nhập Điểm Toán : "))
+# nhập Điểm văn 
+van = int(input("Nhập Điểm Văn : "))
+# nhập điểm tiếng anh 
+anh = int(input("Nhập Điểm Tiếng Anh : "))
+# tính tông điểm 3 môn 
+tong = toan + van + anh 
+# tính trung bình cộng
+tbc = tong / 3 
+# in ra kết quả 
+print("Điểm Trung Bình là : " ,tbc)
+
