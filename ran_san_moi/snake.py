d8b52f97b2242782ab7301702c84f12d754edff4
Your duy <you@example.com>
2025-05-25 18:15:13 +0700

    khanh ran san moi toi chủ nhật

--- ran_san_moi/snake.py
+++ ran_san_moi/snake.py
@@ -2,113 +2,105 @@ import pygame as pg
 import random
 pg.init()
 
-screen = pg.display.set_mode((400,400))
-pg.display.set_caption("snake Game")
+screen = pg.display.set_mode((400, 400))
+pg.display.set_caption("Snake Game")
 
-# var
-sanke_part = 20 
-x=200
-y=200
-
-x_change=y_change = 0
+# Variables
+snake_part = 20
+x = 200
+y = 200
+x_change = y_change = 0
 
 body_snake = []
-length = 1 
+length = 1
 
-# create_fooot
-food_x = random.randint(0,19)*sanke_part
-food_y = random.randint(0,19)*sanke_part
+# Food
+food_x = random.randint(0, 19) * snake_part
+food_y = random.randint(0, 19) * snake_part
 
-# snake speed
+# Clock & Speed
 clock = pg.time.Clock()
 speed = 5
 
-# def fun
+# Scores
+score = 0
+high_score = 0
+
+# Game state
+gameplay = True
+
+# Functions
 def check_vc():
-    if x<0 or x>400 or y<0 or y>400 or(x,y) in body_snake[:-1]:
+    if x < 0 or x >= 400 or y < 0 or y >= 400 or (x, y) in body_snake[:-1]:
         return False
     return True
 
-score=hight_score=0
 def score_view():
-    font=pg.font.Font(None,36)
+    font = pg.font.Font(None, 36)
     if gameplay:
-        score_txt =font.render(f'Score :{score}',True,(255,255,255))
-        screen.blit(score_txt,(0,0))
-        hscore_txt =font.render(f'hightScore :{hight_score}',True,(255,255,255))
-        screen.blit(hscore_txt,(170,0))
-
+        score_txt = font.render(f'Score: {score}', True, (255, 255, 255))
+        screen.blit(score_txt, (10, 10))
+        hscore_txt = font.render(f'High Score: {high_score}', True, (255, 255, 255))
+        screen.blit(hscore_txt, (200, 10))
     else:
-        note_txt =font.render(f'Press space to play a game :{score}',True,(255,255,255))
-        screen.blit(note_txt,(0,0))
+        note_txt = font.render('Press SPACE to play again', True, (255, 255, 255))
+        screen.blit(note_txt, (30, 180))
 
-
-gameplay = True
+# Game loop
 while True:
     for e in pg.event.get():
         if e.type == pg.QUIT:
             pg.quit()
-        # snake_move
+            exit()
         if e.type == pg.KEYDOWN:
-            if e.key==pg.K_LEFT:
-                x_change-=sanke_part
-                y_change=0
-        
-            elif e.key == pg.K_RIGHT:
-                x_change+=sanke_part
-                y_change=0
-            
-            elif e.key == pg.K_UP:
-                x_change=0
-                y_change-=sanke_part
-            elif e.key == pg.K_DOWN:
-                x_change=0
-                y_change+=sanke_part
+            if e.key == pg.K_LEFT and x_change == 0:
+                x_change = -snake_part
+                y_change = 0
+            elif e.key == pg.K_RIGHT and x_change == 0:
+                x_change = snake_part
+                y_change = 0
+            elif e.key == pg.K_UP and y_change == 0:
+                x_change = 0
+                y_change = -snake_part
+            elif e.key == pg.K_DOWN and y_change == 0:
+                x_change = 0
+                y_change = snake_part
             elif e.key == pg.K_SPACE:
-                gameplay=True
-            
+                if not gameplay:
+                    # Reset
+                    x = 200
+                    y = 200
+                    x_change = y_change = 0
+                    body_snake = []
+                    length = 1
+                    score = 0
+                    gameplay = True
 
-    # clear screen
-    screen.fill((0,0,0))
+    screen.fill((0, 0, 0))
     score_view()
+
     if gameplay:
-        # update pisition sanke_part
-        x+=x_change
-        y+=y_change
-        # add snake part
-        body_snake.append((x,y))
-        # remvo snake
-        if len(body_snake)> length:
+        x += x_change
+        y += y_change
+
+        body_snake.append((x, y))
+        if len(body_snake) > length:
             del body_snake[0]
-        # check snake ear foood
-        if x==food_x and y==food_y:
-            length+=1
-            score+=1
-            if score>hight_score:hight_score=score
-            # ramdum lại mồi 
-            food_x = random.randint(0,19)*sanke_part
-            food_y = random.randint(0,19)*sanke_part
-        # vẽ lại thân rắn
-        for x,y in body_snake:
-            pg.draw.rect(screen,(255,255,255),(x,y,sanke_part,sanke_part))
-        # draw foood
-            pg.draw.rect(screen,(255,0,0),(food_x,food_y,sanke_part,sanke_part))
 
-        gameplay=check_vc()
-    else:
-            # reset
-            x=200
-            y=200
-            x_change=y_change = 0
-            body_snake = []
-            length=1
-            score=1
-               
-        
+        if x == food_x and y == food_y:
+            length += 1
+            score += 1
+            if score > high_score:
+                high_score = score
+            food_x = random.randint(0, 19) * snake_part
+            food_y = random.randint(0, 19) * snake_part
 
+        for part in body_snake:
+            pg.draw.rect(screen, (255, 255, 255), (part[0], part[1], snake_part, snake_part))
 
-           
+        pg.draw.rect(screen, (255, 0, 0), (food_x, food_y, snake_part, snake_part))
 
+        gameplay = check_vc()
 
     pg.display.update()
     clock.tick(speed)
